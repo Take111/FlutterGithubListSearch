c7b564ccaaa421edf30135bff8d6ad23178f9be9
ChikatoTakenouchi <maxmaxastea@gmail.com>
2022-09-12 08:12:27 +0900

    feat: StartPageと匿名ログインを導入した

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:github_searcher_flutter/screen/github_list_page.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:github_searcher_flutter/screen/start_page.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
 import 'firebase_options.dart';
 
 void main() async {
@@ -11,21 +12,7 @@ void main() async {
   );
   runApp(
     const ProviderScope(
-      child: MyApp(),
+      child: StartPage(),
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const GithubListPage(),
-    );
-  }
 }
---
+++ lib/screen/start_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:github_searcher_flutter/model/repository/auth_repository.dart';
+import 'package:github_searcher_flutter/screen/github_list_page.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class StartPage extends HookConsumerWidget {
+  const StartPage({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    useEffect(() {
+      WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+        ref.read(signInWithAnnoymouslyProvider).signIn();
+      });
+    });
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const GithubListPage(),
+    );
+  }
+}
